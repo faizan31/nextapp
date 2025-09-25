87e1ce0a5c9bb94926085d64ccd935aa3b96bbc0
faizan <faizan31@gmail.com>
2025-09-25 15:26:21 +0500

    db change

--- app/zaksite/utils/db.ts
+++ app/zaksite/utils/db.ts
@@ -1,19 +1,8 @@
-import mysql from "mysql2/promise";
 
-export async function getConnection() {
-  const connection = await mysql.createConnection({
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME, // your db name
-       port:  3306,
-       
-      // uri: process.env.DATABASE_URL as string,
-    /*sssl: {
-      rejectUnauthorized: false, // some providers require SSL
-    },*/
-  });
+import { createClient } from '@supabase/supabase-js'
 
-  return connection;
-}
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
