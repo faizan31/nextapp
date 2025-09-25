13817180d936379625a7b628539cee5b22cc45f4
faizan <faizan31@gmail.com>
2025-09-25 15:28:54 +0500

    move to old database

--- app/zaksite/utils/db.ts
+++ app/zaksite/utils/db.ts
@@ -1,8 +1,19 @@
+import mysql from "mysql2/promise";
 
-import { createClient } from '@supabase/supabase-js'
+export async function getConnection() {
+  const connection = await mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME, // your db name
+       port:  3306,
+       
+      // uri: process.env.DATABASE_URL as string,
+    /*sssl: {
+      rejectUnauthorized: false, // some providers require SSL
+    },*/
+  });
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
+  return connection;
+}
